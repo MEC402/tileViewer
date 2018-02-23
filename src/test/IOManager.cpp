1c404a990d42ff00437c80a8336e660eb23908bf
Matt Smith <MattSmith11@boisestate.edu>
2018-02-23 13:33:58 -0700

    Copied the test directory to a src folder. Created a new Solution at the top level of the repo

---
+++ src/test/IOManager.cpp
@@ -0,0 +1,71 @@
+#include "IOManager.h"
+#include <fstream>
+
+
+bool IOManager::readBinaryFileToBuffer(
+	const std::string& FILE_PATH,
+	std::vector<unsigned char>& buffer
+) {
+	// Read file as binary
+	std::ifstream file(FILE_PATH, std::ios::binary);
+	if (file.fail()) {
+		perror(FILE_PATH.c_str());
+		return false;
+	}
+
+	// Find file length
+	file.seekg(0, std::ios::end);
+	int fileSize(static_cast<int>(file.tellg()));
+	file.seekg(0, std::ios::beg);
+	fileSize -= static_cast<int>(file.tellg());
+
+	// Fill buffer
+	buffer.resize(fileSize);
+	file.read((char *)&(buffer[0]), fileSize);
+
+	// Close file
+	file.close();
+	return true;
+}
+
+bool IOManager::readTextFileToString(
+	const std::string& FILE_PATH,
+	std::string& fileContents
+) {
+	// Open the file
+	std::ifstream file(FILE_PATH);
+	if (file.fail()) {
+		perror(FILE_PATH.c_str());
+		return false;
+	}
+
+	// Copy file
+	fileContents = "";
+	std::string line;
+	while (std::getline(file, line)) {
+		fileContents += line + "\n";
+	}
+
+	// Close file
+	file.close();
+	return true;
+}
+
+bool IOManager::overwriteStringToFile(
+	const std::string& FILE_PATH,
+	std::string& string
+) {
+	// Open the file
+	std::ofstream file(FILE_PATH);
+	if (file.fail()) {
+		perror(FILE_PATH.c_str());
+		return false;
+	}
+
+	// Write file
+	file << string;
+
+	// Close file
+	file.close();
+	return true;
+}
---
+++ src/test/IOManager.h
@@ -0,0 +1,47 @@
+#pragma once
+#include <vector>
+#include <string>
+
+
+/* Used to load input files as a static class.
+*/
+class IOManager {
+	
+public:
+		
+	/* Reads a binary file into a vector of chars.
+	@param filePath The path to the file to load.
+	@param buffer The vector to fill.
+	@return  If the file read was successful
+	*/
+	static bool readBinaryFileToBuffer(
+		const std::string& filePath,
+		std::vector<unsigned char>& buffer
+	);
+		
+	/* Reads a binary file into a string.
+	@param filePath The path to the file to load.
+	@param fileContents The string to fill.
+	@return If the file read was successful
+	*/
+	static bool readTextFileToString(
+		const std::string& filePath, 
+		std::string& fileContents
+	);
+		
+	/* Overwrite a string to a file.
+	@param filePath The path to the file to overwrite.
+	@param string The string to write.
+	@return If the file write was successful
+	*/
+	static bool overwriteStringToFile(
+		const std::string& FILE_PATH,
+		std::string& string
+	);
+
+private:
+		
+	/* Constructor
+	*/
+	inline IOManager() {};
+};
