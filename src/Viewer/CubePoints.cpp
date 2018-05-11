d3d86b62161e49654c9d9211fc68f8d74263f424
Michael Burke <koenigkrote@outlook.com>
2018-05-11 13:05:34 -0600

    Factor 3d point field out into CubePoints class

--- src/Viewer/CubePoints.cpp
+++ src/Viewer/CubePoints.cpp
@@ -1,14 +1,135 @@
 #include "stdafx.h"
-
+#include <time.h>
 
 CubePoints::CubePoints(int maxResDepth) : m_maxResDepth(maxResDepth) {
 	m_faceDimensions = maxResDepth + 1;
 	m_faceQuads = m_faceDimensions * m_faceDimensions;
-	m_numDataPoints = m_faceQuads * 6 * 6;
-	m_positions.resize(m_faceQuads);
-	m_uvs.resize(m_faceQuads);
-	m_NumVertices = (GLsizei)m_numDataPoints;
-	
+	m_positions.resize(6 * m_faceQuads * m_datasize);
+	m_perRow = m_faceDimensions * m_datasize;
+	m_faceDistance = TILESTEP * ((float)m_faceDimensions / 2.0f);
+	m_NumVertices = (GLuint)(6 * m_faceQuads);
+	srand(time(NULL));
+
+
+	// NOTE: This is set to only generate 4 faces right now, for debugging purposes
+	// ######################### pay attention to me so we don't forget about this later ####################
+	for (int face = 0; face < 4; ++face) {
+		int faceBegin = (m_faceQuads * m_datasize) * face;
+		int faceEnd = (m_faceQuads * m_datasize) * (face + 1);
+
+		float quadX = -TILEWIDTH;
+		float quadY = TILEWIDTH;
+		float xOffset = 0.0f;
+		float yOffset = 0.0f;
+
+		// Point positional data
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+
+		// Geometry "build quad on these points" data
+		float g_x = 0.0f;
+		float g_y = 0.0f;
+		float g_z = 0.0f;
+
+		for (int quadPoint = faceBegin; quadPoint < faceEnd;) {
+
+			// Pretty much just repeated what Cube.cpp() does when generating triangle quads, except the geometry
+			// shader appears to solve the issue of culling triangles for us so mirrored faces don't need flipped coordinates
+			switch (face) {
+			case 0: // Front face
+				x = quadX + xOffset;
+				y = quadY + yOffset;
+				z = -m_faceDistance;
+				g_x = TILEWIDTH;
+				g_y = TILEWIDTH;
+				g_z = 0.00f;
+				break;
+			case 1: // Back face
+				x = quadX + xOffset; // -0.3f; // Debug value to make sure we're actually drawing these guys (we are)
+				y = quadY + yOffset;
+				z = m_faceDistance;
+				g_x = TILEWIDTH;
+				g_y = TILEWIDTH;
+				g_z = 0.00f;
+				break;
+			case 2: // Right face
+				x = m_faceDistance;
+				y = quadY + yOffset;
+				z = quadX + xOffset;
+				g_x = 0.00f; //0.10f;
+				g_y = TILEWIDTH;
+				g_z = TILEWIDTH;
+				break;
+			case 3: // Left face
+				x = -m_faceDistance;
+				y = quadY + yOffset;
+				z = quadX + xOffset;
+				g_x = 0.00f;
+				g_y = 0.05f;
+				g_z = 0.05f;
+				break;
+			case 4: // Top face
+				x = quadX + xOffset;
+				y = m_faceDistance;
+				z = quadY + yOffset;
+				g_x = TILEWIDTH;
+				g_y = 0.00f;
+				g_z = TILEWIDTH;
+				break;
+			case 5:
+				x = quadX + xOffset;
+				y = -m_faceDistance;
+				z = quadY + yOffset;
+				g_x = TILEWIDTH;
+				g_y = 0.00f;
+				g_z = TILEWIDTH;
+				break;
+			}
+
+			// xyz coordinates for the center point of the quad
+			m_positions[quadPoint++] = x;
+			m_positions[quadPoint++] = y;
+			m_positions[quadPoint++] = z;
+
+			// Random colors so we can see if we're generating the right number of quads per face
+			// This will later be replaced with uv maps
+			m_positions[quadPoint++] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+			m_positions[quadPoint++] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+			m_positions[quadPoint++] = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+
+			// What planes we want to create the quad on in our geometry shader
+			m_positions[quadPoint++] = g_x;
+			m_positions[quadPoint++] = g_y;
+			m_positions[quadPoint++] = g_z;
+
+			xOffset += TILESTEP;
+			if (quadPoint != 0 && quadPoint % m_perRow == 0) {
+				xOffset = 0.0f;
+				yOffset -= TILESTEP;
+			}
+		}
+	}
+
+	//unsigned int VBO, VAO;
+	//glGenBuffers(1, &VBO);
+	//glGenVertexArrays(1, &VAO);
+	//glBindVertexArray(VAO);
+	//glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//glBufferData(GL_ARRAY_BUFFER, pointdata.size() * sizeof(float), &pointdata.front(), GL_STATIC_DRAW);
+
+	//// Bind our xyz
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, datasize * sizeof(float), 0);
+
+	//// Bind our rgb
+	//glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, datasize * sizeof(float), (void*)(3 * sizeof(float)));
+
+	//// Bind our geometry plane
+	//glEnableVertexAttribArray(2);
+	//glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, datasize * sizeof(float), (void*)(6 * sizeof(float)));
+	//glBindVertexArray(0);
 	
 	m_setupOGL();
 }
@@ -37,19 +158,30 @@ void CubePoints::m_setupOGL() {
 	// Binding this will create a vertex buffer in your GPU
 	// https://www.opengl.org/sdk/docs/man/html/glGenBuffers.xhtml
 	glGenBuffers(1, &m_PositionVBOID);
+	glGenVertexArrays(1, &m_PositionVAOID);
+	glBindVertexArray(m_PositionVAOID);
+
 	// "Open" a named buffer object
 	// https://www.opengl.org/sdk/docs/man/html/glBindBuffer.xhtml
 	glBindBuffer(GL_ARRAY_BUFFER, m_PositionVBOID);
+
 	// Give data
 	// https://www.opengl.org/sdk/docs/man/html/glBufferData.xhtml
-	/*GLfloat* tempArray = new GLfloat[m_positions.size() * 3];
-	for (int i = 0; i < m_positions.size(); i++) {
-	tempArray[3*i] = m_positions.data()[i].x;
-	tempArray[3*i + 1] = m_positions.data()[i].y;
-	tempArray[3*i + 2] = m_positions.data()[i].z;
-	}
-	*/
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Position) * m_positions.size(), m_positions.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * m_positions.size(), &m_positions.front(), GL_STATIC_DRAW);
+
+	// Bind our xyz
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, m_datasize * sizeof(float), 0);
+
+	// Bind our rgb
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, m_datasize * sizeof(float), (void*)(3 * sizeof(float)));
+
+	// Bind our geometry plane
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, m_datasize * sizeof(float), (void*)(6 * sizeof(float)));
+	glBindVertexArray(0);
+
 	// Enable generic vertex attribute arrays
 	// https://www.opengl.org/sdk/docs/man/html/glEnableVertexAttribArray.xhtml
 	//glEnableVertexAttribArray(0);
--- src/Viewer/CubePoints.h
+++ src/Viewer/CubePoints.h
@@ -11,17 +11,29 @@ public:
 
 	void doubleUVs();
 	GLuint m_PositionVBOID{ 0 };
+	GLuint m_PositionVAOID{ 0 };
 	GLsizei m_NumVertices{ 0 };
 
 private:
 
 	int m_maxResDepth{ 0 };
 	int m_currentResDepth{ 0 };
+
+
+	// Magic hardcoded number please do not forget about me
+	int m_datasize{ 9 };
+	// No really don't forget about it look right here
+
+
 	int m_faceDimensions{ 0 };
 	int m_faceQuads{ 0 };
-	int m_numDataPoints{ 0 };
+	int m_perRow{ 0 };
+	float m_faceDistance{ 0.0f };
 
-	std::vector<Position> m_positions;
+	const float TILEWIDTH = 0.05f;
+	const float TILESTEP = 0.1f;
+
+	std::vector<float> m_positions;
 	std::vector<UVCords> m_uvs;
 	GLuint m_VAOID{ 0 };
 	GLuint m_UVCordsVBOID{ 0 };
